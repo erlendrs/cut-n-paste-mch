5dff3aca03f54c6609380abe19bfb01a699d11ca
Erlend Rånes Salte <erlendranessalte@Erlends-MacBook-Air.local>
2021-10-18 18:55:35 +0200

    endret kode

--- clipboard.py
+++ clipboard.py
@@ -24,13 +24,10 @@ def main():
         if result:
             if "GET_TEXT" in result:
                 df = pd.read_csv(StringIO(result.get("GET_TEXT")),names=['Mch Code'])
-                #txt = df['Mch Code']#';'.join(set(df['Mch Code'].apply(str)))
-                #txt = df.values.tolist()
-                values = ['Mch Code', 'Dokumentnummer']
+                values = ['Mch Code', 'MchKode', 'Dokumentnummer']
                 df = df[df['Mch Code'].isin(values) == False]
                 merged_text = ';'.join(set(df['Mch Code'].apply(str)))
                 st.success(merged_text)
-                #st.text(txt)
                 st.table(df)
                 
     except KeyError as missing_column:
